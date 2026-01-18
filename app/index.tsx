3c034f29abee2f0932f26c2d4cee5f0e8c5878f2
maze02 <mariasb2@gmail.com>
2026-01-18 19:56:01 +0100

    #feature-2: Add  Not Found screen and a navigation link to an About page from the home screen.

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Link } from 'expo-router';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -6,19 +7,28 @@ export default function Index() {
       style={styles.container}
     >
       <Text style={styles.text}>Home screen</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About screen
+      </Link>
     </View>
   );
 }
 
-const styles=StyleSheet.create({
-  container:{
-    flex:1, 
-    backgroundColor:'#25292e',
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text:{
+  text: {
     color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff'
   }
+
 })
 
