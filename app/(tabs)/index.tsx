77916104b0e05b6d744bd728d0705783164a92f3
maze02 <mariasb2@gmail.com>
2026-01-19 16:19:33 +0100

    #feature-10: Implement web image saving functionality and add `sticker-smash.png` asset.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,8 @@
+import domtoimage from 'dom-to-image';
 import * as ImagePicker from 'expo-image-picker';
 import * as MediaLibrary from 'expo-media-library';
 import { useEffect, useRef, useState } from 'react';
-import { ImageSourcePropType, StyleSheet, View } from "react-native";
+import { ImageSourcePropType, Platform, StyleSheet, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { captureRef } from 'react-native-view-shot';
 
@@ -16,60 +17,80 @@ import ImageViewer from '@/components/ImageViewer';
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
-  const imageRef = useRef<View>(null)
-  const [permissionResponse, requestPermission] = MediaLibrary.usePermissions()
-  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined)
-  const [showAppOptions, setShowAppOptions] = useState<boolean>(false)
-  const [isModalVisible, setIsModalVisible] = useState<boolean>(false)
-  const [pickedEmoji, setPickedEmoji] = useState<ImageSourcePropType | undefined>(undefined)
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [pickedEmoji, setPickedEmoji] = useState<ImageSourcePropType | undefined>(undefined);
+  const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
+
+  const imageRef = useRef<View>(null);
 
   useEffect(() => {
     if (!permissionResponse?.granted) {
-      requestPermission()
+      requestPermission();
     }
-  }, [])
+  }, []);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ['images'],
       allowsEditing: true,
       quality: 1,
-    })
+    });
 
     if (!result.canceled) {
-      setSelectedImage(result.assets[0].uri)
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
     } else {
-      alert('You did not select any image.')
+      alert('You did not select any image.');
     }
-  }
+  };
 
   const onReset = () => {
-    setShowAppOptions(false)
-  }
+    setShowAppOptions(false);
+  };
 
   const onAddSticker = () => {
-    setIsModalVisible(true)
-  }
+    setIsModalVisible(true);
+  };
 
   const onModalClose = () => {
-    setIsModalVisible(false)
-  }
+    setIsModalVisible(false);
+  };
 
   const onSaveImageAsync = async () => {
-    try {
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1,
-      })
+    if (Platform.OS !== 'web') {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
 
-      await MediaLibrary.saveToLibraryAsync(localUri)
-      if (localUri) {
-        alert('Saved!')
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert('Saved!');
+        }
+      } catch (e) {
+        console.log(e);
       }
-    } catch (e) {
-      console.log(e)
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement('a');
+        link.download = 'sticker-smash.jpeg';
+        link.href = dataUrl;
+        link.click();
+      } catch (e) {
+        console.log(e);
+      }
     }
-  }
+  };
+
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
@@ -103,11 +124,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   imageContainer: {
     flex: 1,
-    paddingTop: 28
   },
   footerContainer: {
     flex: 1 / 3,
@@ -115,11 +135,10 @@ const styles = StyleSheet.create({
   },
   optionsContainer: {
     position: 'absolute',
-    bottom: 80
+    bottom: 80,
   },
   optionsRow: {
     alignItems: 'center',
-    flexDirection: 'row'
-  }
-})
-
+    flexDirection: 'row',
+  },
+});
