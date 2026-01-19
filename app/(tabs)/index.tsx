31ac66178302c9107fcf1198af9ad5af834f6b12
maze02 <mariasb2@gmail.com>
2026-01-19 12:04:06 +0100

    feature-4: add ImageViewer and Button components and update the home screen to display an image and interactive buttons.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,18 @@
-import { Link } from 'expo-router';
-import { StyleSheet, Text, View } from "react-native";
+import Button from '@/components/Button';
+import ImageViewer from '@/components/ImageViewer';
+import { StyleSheet, View } from "react-native";
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
-    <View
-      style={styles.container}
-    >
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
@@ -18,17 +21,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
+    alignItems: 'center'
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 28
+  },
+  footerContainer: {
+    flex: 1 / 3,
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff'
   }
-
 })
 
